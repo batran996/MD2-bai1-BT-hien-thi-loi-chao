d6074cbb7993d3ebcb3448b952ad9f727367b230
unknown <bazo20061996@gmail.com>
2022-07-25 11:18:31 +0700

    MD2-bai1-BT-loi chao

---
+++ src/HienThiLoiChao.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        System.out.println("enter your name ");
+        Scanner ten = new Scanner(System.in);
+        String name = ten.nextLine();
+        System.out.println("hello " +name);
+
+    }
+}
